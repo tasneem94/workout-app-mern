067a3f836d05bef0d2036320996988699ba2249f
tasneem94 <tasneemsakif31@gmail.com>
2024-04-16 16:28:16 +0600

    Added Delete func

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,20 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  const handleClick = async () => {
+    const response = await fetch(
+      `http://localhost:4000/api/workouts/${workout._id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: data });
+    }
+  };
   return (
     <div className="workout-details">
       <h3>{workout.title}</h3>
@@ -11,6 +27,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 };
