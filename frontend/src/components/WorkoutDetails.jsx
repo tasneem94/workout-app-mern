250995b8e309b4cff0aa474c21746df5f609201d
tasneem94 <tasneemsakif31@gmail.com>
2024-04-25 22:46:26 +0600

    added env for port

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -11,7 +11,7 @@ const WorkoutDetails = ({ workout }) => {
     }
 
     const response = await fetch(
-      `http://localhost:4000/api/workouts/${workout._id}`,
+      `${process.env.LOCAL_URL}/api/workouts/${workout._id}`,
       {
         method: "DELETE",
         headers: {
